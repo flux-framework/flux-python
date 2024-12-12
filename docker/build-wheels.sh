a76588dfad8fd79899fbcd49b5e2b255d5e2c007
vsoch <vsoch@users.noreply.github.com>
2024-12-12 16:02:41 -0700

    refactor build pipeline for different mamaba versions
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- docker/build-wheels.sh
+++ docker/build-wheels.sh
@@ -2,10 +2,17 @@
 
 here=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 build_number=${1:-0}
+version=${2:-3.11}
 
-# Create python environments for each version we want to release!
-# We wrap like this because this script can be used locally, but
-# in GitHub actions we want to do in a matrix
-for version in 3.6 3.7 3.8 3.9 3.10; do
-    /bin/bash $here/build-wheel.sh ${version} ${build_number}
-done
+# This is intended to run in the container
+echo "Building Python version ${version}"
+
+export PATH=/opt/conda/envs/build/bin:$PATH
+export PYTHONPATH=/opt/conda/envs/build/lib/python${version}/site-packages
+
+/opt/conda/bin/mamba activate build || true
+
+# Build the bindings for this python version!
+/opt/conda/envs/build/bin/python3 setup.py sdist
+/opt/conda/envs/build/bin/python3 setup.py bdist_wheel --plat-name=any --build-number=${build_number}
+unset PYTHONPATH
